19215e4c77b07abb545b12f193a6ce59b6ad7f5e
Mustafa Undal <undalmustafa@gmail.com>
2024-05-26 13:26:06 +0300

    BUILD: created logger class

--- Logger.h
+++ Logger.h
@@ -1,14 +1,30 @@
-//
-// Created by undal on 5/26/2024.
-//
+#ifndef LOGGER_H
+#define LOGGER_H
 
-#ifndef CIRCULARLOGTASK_LOGGER_H
-#define CIRCULARLOGTASK_LOGGER_H
-
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <string>
+#include <filesystem>
 
 class Logger {
+public:
+    enum LoggingType { DAILY, HOURLY, MINUTELY, SECONDLY };
 
+    Logger(LoggingType type, int frequency, int maxFiles);
+
+    void log(const std::string& message);
+
+private:
+    LoggingType loggingType;
+    int frequency;
+    int maxFiles;
+    std::string currentLogFilePath;
+    std::string logDirectory;
+
+    std::string getCurrentTimestamp();
+    std::string generateLogFileName();
+    void rotateLogFiles();
 };
 
-
-#endif //CIRCULARLOGTASK_LOGGER_H
+#endif // LOGGER_H
