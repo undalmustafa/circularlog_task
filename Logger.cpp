1643fcb72e444a951a25d27993ec282b1393012b
Mustafa Undal <undalmustafa@gmail.com>
2024-05-26 13:14:01 +0300

    project initialized

---
+++ Logger.cpp
@@ -0,0 +1,5 @@
+//
+// Created by undal on 5/26/2024.
+//
+
+#include "Logger.h"
---
+++ Logger.h
@@ -0,0 +1,14 @@
+//
+// Created by undal on 5/26/2024.
+//
+
+#ifndef CIRCULARLOGTASK_LOGGER_H
+#define CIRCULARLOGTASK_LOGGER_H
+
+
+class Logger {
+
+};
+
+
+#endif //CIRCULARLOGTASK_LOGGER_H
